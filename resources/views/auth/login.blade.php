780618e00e6d74ad152259edacd9ddc7647a7b42
Yêda Silva <yedagabriela@live.com>
2022-07-20 21:44:01 -0300

    Update v.4
    Aqui foram feitas modificações no dash board e também foi retirada a página inicial (ocultada).

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,6 +40,14 @@
                 </label>
             </div>
 
+            <div class="block mt-4">
+                <label for="register_me" class="inline-flex items-center">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                        {{ __('Não possuo registro ainda') }}
+                    </a>
+                </label>
+            </div>
+
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,23 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                        <th scope="col">Id</th>
+                        <th scope="col">Nome do Usuário</th>
+                        <th scope="col">e-mail</th>      
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                        <tr>
+                        <th scope="row">1</th>
+                        <td>{{auth()->user()->name}}</td>
+                        <td>{{auth()->user()->email}}</td>      
+                        </tr>
+                        <tr>
+                        
                 </div>
             </div>
         </div>
