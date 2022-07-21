c7f210ef7eecd4330f3e3e8df9fc59190974481d
Yêda Silva <yedagabriela@live.com>
2022-07-20 23:20:43 -0300

    v.6
    Melhorei a tabela do dashboard e retirei a logo do laravel

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,31 +1,30 @@
 <x-app-layout>
-    <x-slot name="header">
+    <!-- <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Painel de controle') }}
         </h2>
-    </x-slot>
+    </x-slot> -->
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                        <th scope="col">Id</th>
-                        <th scope="col">Nome do Usuário</th>
-                        <th scope="col">e-mail</th>      
-                        </tr>
-                    </thead>
-                    <tbody>
-                        
-                        <tr>
-                        <th scope="row">1</th>
-                        <td>{{auth()->user()->name}}</td>
-                        <td>{{auth()->user()->email}}</td>      
-                        </tr>
-                        <tr>
-                        
+                    <table class="w-full text-left tracking wider">
+                        <thead class="tracking wider">
+                            <tr>
+                            <th class="px-6 py-3 text-left"><p>Id</p></th>
+                            <th class="px-6 py-3 text-left"><p">Nome do Usuário</p></th>
+                            <th class="px-6 py-3 text-left"><p">e-mail</p></th>
+                            </tr>
+                        </thead>
+                        <tbody class="">
+                            <tr>
+                            <td class="px6 py-4 text-center"><p>{{auth()->user()->id}}</p></td>
+                            <td class="px6 py-4 text-center"><p>{{auth()->user()->name}}</p></td>
+                            <td class="px6 py-4 text-center"><p>{{auth()->user()->email}}</p></td>
+                            </tr>    
+                        </tbody>
+                    </table>                        
                 </div>
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
@@ -21,11 +22,11 @@
             @include('layouts.navigation')
 
             <!-- Page Heading -->
-            <header class="bg-white shadow">
+            <!-- <header class="bg-white shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                     {{ $header }}
                 </div>
-            </header>
+            </header> -->
 
             <!-- Page Content -->
             <main>
